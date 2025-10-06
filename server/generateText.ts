47c2064f10edb22e6792ca6838d859886c73078c
noobtuber20103152 <ujjawalrachhoya100@gmail.com>
2025-10-06 23:48:56 +0530

    remove key

--- server/generateText.ts
+++ server/generateText.ts
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 const client = new OpenAI({
-    apiKey: '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',
+    apiKey: '',
 });
 
 
